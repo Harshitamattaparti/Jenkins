c2602469b31477a2e2c5a3b1acb5c9bdde970433
Harshitamattaparti <84396867+Harshitamattaparti@users.noreply.github.com>
2024-06-15 13:46:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,20 @@
 terraform{
-required_providers{
-aws={
-source="hashicorp/aws"
-version="~> 5.0"
-}
-}
+  required_providers{
+    aws={
+      source="hashicorp/aws"
+      version="~> 5.0"
+    }
+  }
 }
 
 provider="aws"{
-region="us-east-1"
+  region="us-east-1"
 }
 
 resource "aws-instance" "TerraInst"{
-ami= "ami-08a0d1e16fc3f61ea"
-instance_type="t2.micro"
+  ami= "ami-08a0d1e16fc3f61ea"
+  instance_type="t2.micro"
+  tags={
+    Name="TerraInst"
+  }
 }
